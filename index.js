7e2543606885569047cf4305e0bf77aa9855e508
RenanSouza2 <renan.rodrigues.souza@usp.br>
2020-05-19 23:34:48 -0300

    Big change in the List constructor

--- index.js
+++ index.js
@@ -41,181 +41,164 @@ Lazy.prototype._eval_done = function(){return this;}
 
 const exit = {};
 
-function vazio(_list) {return _list.item === exit}
-
-function pred_true() {return true}
-
-function pred_false() {return false}
-
-function pred_equal(reference) {return (item) => {return item === reference}}
-
-function validate_pred(pred) {return ({_list}) => {return vazio(_list) || pred(_list.item)}}
-
-function advance_list({_list}) {return {_list: _list.next}}
-
-function advance_n({_list,_n}){return {_list: _list.next, _n: _n-1}}
-
-function id(func) {
-    if(func === undefined) return (item) => {return item};
-    return (item) => {func(item); return item;};
+const end_list = {
+    _item : exit,
+    _next : null,
+     empty: () => {return true}
 }
+
+function vazio(_list) {return _list.item === exit}
 
 
 
 class List extends Lazy {
-    constructor(_label, _item, {_predicate, _advance, _validate, _apply, _next}) {
-        super(_label, {_item, _functions: {_predicate, _advance, _validate, _apply, _next}},
-            ({_item, _functions: {_predicate, _apply, _validate, _advance, _next}}) => {
-
-                if (_predicate === undefined)
-                if (_item._list.item === exit)
-                    return {_item: exit, _next: null};
-
-                if (_predicate !== undefined)
-                if (_predicate(_item))
-                    return {_item: exit, _next: null};
-
-                let _next_list = null;
-                if(_next !== undefined) _next_list = _next(_item);
-
-                if(_next_list === null)
-                {
-                    if(_advance === undefined) _advance = advance_list;
-                    _next_list = new List(_label, _advance(_item), {_predicate, _advance, _validate, _apply, _next});
-                }
-
-                if (_validate !== undefined)
-                if (!_validate(_item))
-                    return _next_list.evaluate();
-
-                if (_apply === undefined) _item = _item._list.item;
-                else _item = _apply(_item);
-
-                return {_item: _item, _next: _next_list};
-            });
-    }
-
+    constructor(_label,_args,_func) {super(_label,_args,_func);}
     get next() {return this.evaluate()._next}
 }
 
 List.prototype.evaluateAll = function() {
-    if(vazio(this)) return;
+    if(this.empty()) return;
     this.next.evaluateAll();
+}
+
+List.prototype.empty = function() {return this.item === exit}
+
+List.prototype.forEach = function(func){
+    if(this.empty()) return;
+    func(this.item);
+    this.next.forEach(func);
 }
 
 
 
 List.prototype.take = function(n){
-    return new List ('Take', {_list: this, _n: n}, {
-                            _predicate: ({_list, _n}) => {return vazio(_list) || _n === 0},
-                            _advance:   advance_n,
-                        });
+    return new List ('Take', {_list: this, _n: n},function ({_list,_n}) {
+        if(_n === 0)        return end_list;
+        if(_list.empty())   return end_list;
+        return {
+            _item: _list.item,
+            _next: _list.next.take(n-1)
+        }
+    });
 }
 
 List.prototype.map = function(func){
-    return new List ('Map', {_list: this}, {
-            _advance: advance_list,
-            _apply: ({_list}) => {return func(_list.item)}
-        });
+    return new List ('Map', this, function (_list){
+        if(_list.empty()) return end_list;
+        return {
+            _item: func(_list.item),
+            _next: _list.next.map(func)
+        }
+    })
 }
 
 List.prototype.filter = function(pred){
-    return new List ('Filter', {_list: this}, {
-                            _advance: advance_list,
-                            _validate: validate_pred(pred)
-                        });
+    return new List ('Filter', this, function _func(_list) {
+        if(_list.empty()) return end_list;
+        if(pred(_list.item))
+            return {
+                _item: _list.item,
+                _next: _list.next.filter(pred)
+            }
+
+        return _func(_list.next);
+    })
 }
 
 List.prototype.from = function(pred){
-    return new List('From',{_list: this},{
-            _validate: validate_pred(pred),
-            _advance: advance_list,
-            _next: ({_list}) => {
-                if(pred(_list.item)) return _list.next;
-                else                 return null;
+    return new List('From',this,function _func(_list){
+        if(_list.empty()) return end_list;
+        if(pred(_list.item))
+            return {
+                _item: _list.item,
+                _next: _list.next
             }
-        })
+
+        return _func(_list.next);
+    })
 }
 
 List.prototype.until = function(pred){
-    return new List ('Until',{_list: this, }, {
-                            _predicate: validate_pred(pred),
-                            _advance: advance_list
-                        })
+    return new List ('Until', this, function (_list){
+        if(_list.empty())       return end_list;
+        if(pred(_list.item))    return end_list;
+        return {
+            _item: _list.item,
+            _next: _list.next.until(pred)
+        }
+    })
 }
 
 List.prototype.reduceMap = function(func,offset){
-    return new List ('ReduceMap',{_offset:offset,_list:this}, {
-        _predicate: ({_list}) => {return _list === null},
-        _advance:   ({_offset,_list}) => {return {_offset: func(_offset,_list.item), _list: _list.next}},
-        _apply:     ({_offset,_list}) => {return _offset}
-    }).next;
+    return new List ('ReduceMap',{_offset:offset,_list:this}, function ({_offset,_list}){
+        if(_list.empty()) return end_list;
+        _offset = func(_offset,_list.item);
+        return {
+            _item: _offset,
+            _next: _list.next.reduceMap(func,_offset)
+        }
+    })
 }
 
 List.prototype.append = function(list){
-    return new List ('Append',{_list: this, _append: list}, {
-                        _predicate: ({_list}) => {return _list === null},
-                        _advance:   ({_list,_append}) => {return {_list: _list.next,_append:_append}},
-                        _apply:     ({_list, _append}) => {
-                                        if(vazio(_list))    return _append.item;
-                                        else                return _list.item
-                                    },
-                        _next:      ({_list, _append}) => {
-                                        if(vazio(_list))    return _append.next;
-                                        else                return null;
-                                    }
-                        });
+    return new List ('Append',this, function (_list) {
+        if(_list.empty()) return list.evaluate();
+        return {
+            _item: _list.item,
+            _next: _list.next.append(list)
+        }
+    })
 }
 
 List.prototype.update = function (n,item){
-    return new List('Update', {_list: this, _n: n},{
-        _advance: advance_n,
-        _apply: ({_list,_n}) => {
-                    if(_n === 0) return item
-                    else         return _list.item
-                },
-        _next: ({_list,_n}) => {
-                    if(_n === 0) return _list.next;
-                    else         return null;
-                }
+    return new List('Update', {_list: this, _n: n}, function _func({_list,_n}) {
+        if(_list.empty()) return end_list;
+        if(_n === 0)
+            return {
+                _item: item,
+                _next: _list.next
+            }
+        return {
+            _item: _list.item,
+            _next: _list.next.update(n-1,item)
+        }
     })
 }
 
 List.prototype.plain = function(){
-    return new List('Plain',{_list: this, _item: this.item},{
-        _advance:   ({_list,_item}) => {
-                        if(vazio(_item))
-                            return {
-                                _list: _list.next,
-                                _item: _list.next.item
-                            };
-                        return {
-                            _list: _list,
-                            _item: _item.next
-                        };
-                    },
-        _validate:  ({_item}) => {return !vazio(_item)},
-        _apply:     ({_item}) => {return _item.item},
+    return new List('Plain',{_list: this, _item: this.item}, function _func({_list,_item}){
+        if(_item.empty())
+        {
+            _list = _list.next;
+            if(_list.empty()) return end_list;
+
+            return _list.plain();
+        }
+        return {
+            _item: _item.item,
+            _next: new List('Plain',{_list: _list, _item: _item.next},_func)
+        }
     });
 }
 
 List.prototype.tensor = function(list,func){return this.map((item_a)=>{return list.map((item_b)=>{return func(item_a,item_b)})})}
 
 List.prototype.envelope = function() {
-    return new List('Envelope',{_list:this},{
-        _apply: ({_list}) => {return _list}
+    return new List('Envelope',this, function _func(_list){
+        if(_list.empty()) return end_list;
+        return {
+            _item: _list,
+            _next: new List('Envelope',_list.next,_func)
+        }
     })
 }
 
 
 
 List.prototype.access = function(n){
-    const res = new List('Access', {_list: this, _n: n},{
-        _advance:  advance_n,
-        _validate: ({_n}) => {return _n === 0}
-    }).item;
-    if(res === exit) return undefined;
-    return res;
+    if(this.empty()) return undefined;
+    if(n === 0) return this.item;
+    return this.next.access(n-1);
 }
 
 List.prototype.first  = function(pred) {
@@ -224,17 +207,9 @@ List.prototype.first  = function(pred) {
     return res;
 }
 
-List.prototype.reduce = function(func,offset){
-    return new List ('Reduce', {_offset: offset,_list: this},{
-        _predicate: pred_false,
-        _advance:   ({_offset,_list}) => {return {_offset: func(_offset,_list.item),_list: _list.next}},
-        _validate:  ({_list}) => {return _list.item === exit},
-        _apply:     ({_offset}) => {return _offset},
-        _next:      ({_list}) => {
-                        if(vazio(_list)) return exit;
-                        else             return null;
-                    }
-    }).item;
+List.prototype.reduce = function(func,offset) {
+    if (this.empty()) return offset;
+    return this.next.reduce(func, func(offset, this.item));
 }
 
 List.prototype.toArray = function() {
@@ -243,14 +218,7 @@ List.prototype.toArray = function() {
             .reduce((arr1,arr2) => {return [...arr1,...arr2]},[]);
 }
 
-List.prototype.length = function(){
-    return new List('Length',{_list: this, _n: 0},{
-        _predicate: pred_false,
-        _advance:   ({_list,_n}) => {return {_list: _list.next, _n: _n+1}},
-        _validate:  ({_list}) => {return _list.item === exit},
-        _apply:     ({_n}) =>{return _n}
-    }).item;
-}
+List.prototype.length = function(){return this.map(()=>{return 1}).reduce(soma,0)}
 
 List.prototype.displayAll = function(){
     this.display();
@@ -261,43 +229,36 @@ List.prototype.displayAll = function(){
 
 
 function combine(list_a,list_b,func){
-    return new List('Combine', {_list_a: list_a, _list_b: list_b}, {
-        _predicate: ({_list_a, _list_b}) => {return vazio(_list_a) || vazio(_list_b)},
-        _advance:   ({_list_a, _list_b}) => {return {_list_a: _list_a.next, _list_b: _list_b.next}},
-        _apply:     ({_list_a, _list_b}) => {return func(_list_a.item, _list_b.item)}
+    return new List('Combine', {_list_a: list_a, _list_b: list_b}, function _func({_list_a,_list_b}){
+        if(_list_a.empty()) return end_list;
+        if(_list_b.empty()) return end_list;
+
+        return {
+            _item: func(_list_a.item,_list_b.item),
+            _next: combine(_list_a.next,_list_b.next,func)
+        }
     });
 }
 
 function join_ordered(list_a, list_b) {
-    return new List('Join ordered',{_list_a: list_a, _list_b: list_b},{
-        _predicate: pred_false,
-        _advance:   ({_list_a,_list_b}) => {
-            if(_list_a.item < _list_b.item)
-                return {
-                    _list_a: _list_a.next,
-                    _list_b: _list_b
-                };
-            return {
-                _list_a: _list_a,
-                _list_b: _list_b.next
-            };
-        },
-        _apply:     ({_list_a,_list_b}) => {
-            if(_list_a.item < _list_b.item) return _list_a.item;
-            return _list_b.item;
-        },
-        _next:      ({_list_a,_list_b}) => {
-            if(_list_a.item < _list_b.item)
-            {
-                if(vazio(_list_a.next))
-                    return _list_b;
-            }
-            else
-            {
-                if(vazio(_list_b.next))
-                    return _list_a;
-            }
-            return null;
+    return new List('Join ordered',{_list_a: list_a, _list_b: list_b},function (){
+        if(_list_a.empty()) return end_list;
+
+        let menor, maior;
+        if(_list_a.item < _list_b.item)
+        {
+            menor = _list_a;
+            maior = _list_b;
+        }
+        else
+        {
+            menor = _list_b;
+            maior = _list_a;
+        }
+
+        return {
+            _item: menor.item,
+            _next: join_ordered(menor.next,maior)
         }
     })
 }
@@ -317,12 +278,9 @@ function somaHigh(a) {return (b) => {return a+b}}
 function multHigh(a) {return (b) => {return a*b}}
 
 function imediate(item) {
-    return new List('Imediate',item,{
-                                _predicate: pred_false,
-                                _advance:   () => {return null},
-                                _apply:     id(),
-                                _next:      () => {return exit}
-                            }).evaluate();
+    return new List('Imediate', undefined, () => {
+        return {_item: item, _next: end_list}
+    })
 }
 
 function displayAll(_list) {
@@ -336,106 +294,129 @@ function displayAll(_list) {
 
 function test() {
     console.log();
+    console.log('Num');
     console.log([0,1,2,3,4]);
     num(0,5).displayAll();
 
     console.log();
+    console.log('Take 5');
     console.log([0,1,2,3,4]);
     num().take(5).displayAll();
 
     console.log();
+    console.log('Map +1');
     console.log([0,2,4,6,8]);
     num(0,5).map(multHigh(2)).displayAll();
 
     console.log();
+    console.log('Filter par');
     console.log([0,2,4]);
     num(0,5).filter((n) => {return n%2 === 0}).displayAll();
 
     console.log();
+    console.log('Filter >=10');
     console.log('Nada');
     num(0,5).filter(moreEqualThen(10)).displayAll();
 
     console.log();
+    console.log('From >=3');
     console.log([3,4]);
     num(0,5).from(moreEqualThen(3)).displayAll();
 
     console.log();
+    console.log('From 10');
     console.log('Nada');
     num(0,5).from(moreEqualThen(10)).displayAll();
 
     console.log();
+    console.log('Until >=5');
     console.log([0,1,2,3,4]);
     num().until(moreEqualThen(5)).displayAll();
 
     console.log();
     console.log([0,1,2,3,4]);
+    console.log('Until >=10');
     num(0,5).until(moreEqualThen(10)).displayAll();
 
     console.log();
-    console.log([0,1,2,3,4]);
-    num(0,5).until(moreEqualThen(10)).displayAll();
-
-    console.log();
+    console.log('Combine [0...5][10...]');
     console.log([[0,10],[1,11],[2,12],[3,13],[4,14]]);
-    num(0,5).combine(num(10),(item_a,item_b) => {return [item_a,item_b]}).displayAll();
+    combine(num(0,5),num(10),(item_a,item_b) => {return [item_a,item_b]}).displayAll();
 
     console.log();
+    console.log('ReduceMap soma');
     console.log([0,1,3,6,10]);
     num(0,5).reduceMap(soma,0).displayAll();
 
     console.log();
+    console.log('Append[0...5][10...15]');
     console.log([0,1,2,3,4,10,11,12,13,14]);
     num(0,5).append(num(10,15)).displayAll();
 
     console.log();
-    console.log([0,1,2,3,4,10,11,12,13,14]);
-    num(0,5).append(num(10,15)).displayAll();
+    console.log('Update 3 Test');
+    console.log([0,1,2,'Test',4]);
+    num(0,5).update(3,'Test').displayAll();
 
     console.log();
-    console.log([0,1,2,'Teste',4]);
-    num(0,5).update(3,'Teste').displayAll();
-
-    console.log();
+    console.log('Access 3');
     console.log(3);
     console.log(num().access(3));
 
     console.log();
+    console.log('Access 10');
     console.log(undefined);
     console.log(num(0,5).access(10));
 
     console.log();
+    console.log('First >=3');
     console.log(3);
     console.log(num().first(moreEqualThen(3)));
 
     console.log();
+    console.log('First 10');
     console.log(undefined);
     console.log(num(0,5).first(moreEqualThen(10)));
 
     console.log();
+    console.log('Reduce soma');
     console.log(10);
     console.log(num(0,5).reduce(soma,0));
 
     console.log();
+    console.log('To array');
     console.log([0,1,2,3,4]);
     console.log(num(0,5).toArray());
 
     console.log();
+    console.log('Length');
     console.log(5);
     console.log(num(0,5).length());
 }
 
 
 
-function num(n1,n2) {
+function num(n1, n2) {
     if(n1 === undefined) n1 = 0;
-    let _predicate;
-    if(n2 === undefined) _predicate = pred_false;
-    else                 _predicate = moreEqualThen(n2);
-    return new List('Num',n1, {
-                        _predicate: _predicate,
-                        _advance: somaHigh(1),
-                        _apply: id()
-                    });
+
+    let func;
+    if(n2 === undefined)
+    func = function _func(n){
+        return {
+            _item: n,
+            _next: new List('Num',n+1,_func)
+        }
+    }
+    else
+    func = function _func(n) {
+        if(n === n2) return end_list;
+        return {
+            _item: n,
+            _next: new List('Num',n+1,_func)
+        }
+    }
+
+    return new List('Num',n1,func);
 }
 
 function sieveLazy(list, list_filter) {
@@ -513,8 +494,6 @@ function megaSieve(){
     return toLazy([2,3]).append(proximo);
 }
 
-//displayAll(megaSieve());26748181
-
 function getTime(){return new Date().getTime()}
 
 function ShowTime(){
@@ -534,4 +513,14 @@ function ShowTime(){
     displayAllTime(sieve().until(moreEqualThen(26285670)), 10000);
 }
 
+function generate_coprimes(list,lim) {
+    let pred = moreEqualThen(lim);
+    list = list.until(pred);
+    return combine(list,list.envelope(),(item_a,item_b) => {
+        return item_b.map(multHigh(item_a)).until(pred);
+    }).until((item)=>{return item.empty()})
+}
 
+let n = num(1);
+
+generate_coprimes(n,100).forEach((item)=>{item.displayAll()});
